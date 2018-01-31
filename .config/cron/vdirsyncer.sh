ccf001851fc7f6d1ff31d9c8d70615b5280eaa3f
Daniel Moch <daniel@danielmoch.com>
2018-01-30 19:04:59 -0500

    vdirsyncer.sh: Use xsession.pid to check for running X server

--- .config/cron/vdirsyncer.sh
+++ .config/cron/vdirsyncer.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 echo "Beginning sync on `date`"
-export DISPLAY=`cat $HOME/.xdisplay`
-vdirsyncer sync
-unset DISPLAY
+xsession_pid=`cat $HOME/.xsession.pid`
+
+if ps -U $LOGNAME -o pid= | grep $xsession_pid > /dev/null 2>&1 && type vdirsyncer > /dev/null 2>&1
+then
+    export DISPLAY=`cat $HOME/.xdisplay`
+    vdirsyncer sync
+    unset DISPLAY
+else
+    echo "No X session running. Aborting"
+fi
+
 echo "Completed sync on `date`"
