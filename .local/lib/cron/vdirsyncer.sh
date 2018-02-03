65138bcf6cf0022346206a4f0348d730810c8d24
Daniel Moch <daniel@danielmoch.com>
2018-02-03 08:08:24 -0500

    Use netrc for service passwords
    The keyring API requires a running DBus, which I don't want to be
    constrained to, especially for cron jobs. Python has a nice netrc API
    already, so we just expose that to the command line and allow any
    service with password eval capability to use it.

--- .local/lib/cron/vdirsyncer.sh
+++ .local/lib/cron/vdirsyncer.sh
@@ -1,14 +1,4 @@
 #!/bin/sh
 echo "Beginning sync on `date`"
-xsession_pid=`cat $HOME/.xsession.pid`
-
-if ps -U $LOGNAME -o pid= | grep $xsession_pid > /dev/null 2>&1 && type vdirsyncer > /dev/null 2>&1
-then
-    export DISPLAY=`cat $HOME/.xdisplay`
-    vdirsyncer sync
-    unset DISPLAY
-else
-    echo "No X session running. Aborting"
-fi
-
+vdirsyncer sync
 echo "Completed sync on `date`"
