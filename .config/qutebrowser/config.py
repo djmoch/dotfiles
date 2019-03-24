45776fa95c50e4cb6bff97a9bdc47fa3df6a6d75
Daniel Moch <daniel@danielmoch.com>
2019-03-24 08:25:26 -0400

    Update Qutebrowser config

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -23,7 +23,7 @@ c.url.default_page = 'https://archlinux.org'
 c.url.start_pages = ['https://mastodon.technology',
                      'https://twitter.com',
                      'https://archlinux.org']
-c.url.searchengines = { 'DEFAULT': 'https://duckduckgo.com/html/?q={}' }
+c.url.searchengines = { 'DEFAULT': 'https://duckduckgo.com/?q={}' }
 
 config.bind('<z><l>', "spawn --userscript qute-pass -u '^user.*:\s(.*)$' -U secret -d dmenu")
 config.bind('<Ctrl-n>', 'completion-item-focus next', mode="command")
@@ -34,6 +34,7 @@ config.bind('<z><c>', 'config-edit')
 config.bind('<,><z><c>', 'spawn -v my term -e ' + editor + ' .config/qutebrowser/config_local.py')
 
 # JavaScript whitelist
+config.set('content.javascript.enabled', True, 'https://duckduckgo.com')
 config.set('content.javascript.enabled', True, 'https://mastodon.technology')
 config.set('content.javascript.enabled', True, 'https://twitter.com')
 config.set('content.javascript.enabled', True, 'https://github.com')
