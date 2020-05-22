244b3c925cfadb7823030a39df84f76b1f4f4b73
Daniel Moch <daniel@danielmoch.com>
2020-05-22 10:04:30 -0400

    cd.sh: Smarter environment detection

--- etc/shrc.d/cd.sh
+++ etc/shrc.d/cd.sh
@@ -1,10 +1,8 @@
-# The issue here is that Acme requires an additional call coupled
-# with cd in order to update the window tag. We can use the value
-# of $EDITOR to see if we're in an Acme environment.
-if [ "$EDITOR" = "E" ]
+if [ "$termprog" = "9term" -o "$termprog" = "win" ]
 then
 	cd() {
 		[ -z "$1" ] && set -- "$HOME"
 		command cd "$1" && awd $HOSTNAME
 	}
+	awd $HOSTNAME
 fi
