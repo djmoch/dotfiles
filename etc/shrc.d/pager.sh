bc4eec6ce9518776e926b8390d986db891c939ca
Daniel Moch <daniel@danielmoch.com>
2020-06-17 16:17:28 -0400

    pager: Handle dumb terminals

--- etc/shrc.d/pager.sh
+++ etc/shrc.d/pager.sh
@@ -1,8 +1,16 @@
-if type less > /dev/null 2>&1
+if [ "$TERM" = "dumb" ]
+then
+	if type nobs >/dev/null 2>&1
+	then
+		PAGER=nobs
+	else
+		PAGER=cat
+	fi
+elif type less >/dev/null 2>&1
 then
 	PAGER=less
 	LESS="-FMRqX#10"; export LESS
-	if type lesspipe > /dev/null 2>&1
+	if type lesspipe >/dev/null 2>&1
 	then
 		eval `lesspipe`
 	else
